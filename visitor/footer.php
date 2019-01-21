8c7e188d9de373b020850445b4e09db40d435654
miloshosky <aleksandar.miloshoski@outlook.com>
2019-01-21 19:36:37 +0100

    added css for the visitor view, footer and pictures for testing

---
+++ visitor/footer.php
@@ -0,0 +1,5 @@
+			<script src="../admin/vendor/jquery/jquery.min.js"></script>
+			<script src="../admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="../admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+		</body>
+	</html>
